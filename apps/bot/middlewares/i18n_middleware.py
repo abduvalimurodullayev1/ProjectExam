3d924fbbfff5aed5f7b9e661d294df26b66e1d0c
Abduvali Murodullayev <125810900+abduvalimurodullayev1@users.noreply.github.com>
2024-12-20 17:53:59 +0500

    Update i18n_middleware.py

--- apps/bot/middlewares/i18n_middleware.py
+++ apps/bot/middlewares/i18n_middleware.py
@@ -1,9 +1,8 @@
 from typing import Dict, Any
-from asgiref.sync import sync_to_async
-
 from aiogram.types.base import TelegramObject
 from aiogram.utils.i18n import I18nMiddleware
 from aiogram import types
+from asgiref.sync import sync_to_async
 from django.core.cache import cache
 
 from apps.bot.models import User
@@ -24,13 +23,11 @@ class CustomI18nMiddleware(I18nMiddleware):
         user_language = cache.get(cache_key)
 
         if not user_language:
-            # Bazadan ma'lumotni olish
             user_obj = await User.objects.filter(telegram_id=telegram_id).afirst()
             if user_obj and user_obj.language:
                 user_language = user_obj.language
-                # Keshga yozish
-                await sync_to_async(cache.set)(cache_key, user_language, timeout=3600)  # 1 soat muddat
+                await sync_to_async(cache.set)(cache_key, user_language, timeout=3600)
             else:
-                user_language = self.i18n.default_locale  # Agar user yoki til topilmasa, default til
+                user_language = self.i18n.default_locale
 
         return user_language
